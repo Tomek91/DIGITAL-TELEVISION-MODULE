b6ea4bf358fc73d8e3974a163bb8b1e8f0bcde9c
Tomek91 <tomek.r9@wp.pl>
2020-01-05 16:16:02 +0100

    README aktualizacja

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DIGITAL TELEVISION MODULES
+
+
+Aplikacja do symulowania pracą telewizji cyfrowej. Dane o kanałach jest pobierane z zewnętrznego api: DigitalTelevisionApiMultiModule. Użytkownik ma tutaj możliwość zakupienia kanałów, które są pogrupowane tematycznie w pakiety, na mail są wysyłane rachunki, faktury do zapłacenia, wiadomości o nowościach / reklamach, oraz sugerowane pakiety na podstawie preferencji użytkownika.
+
+
+<b>Stack technologiczny:</b> Spring Boot REST, Jpa, Security, Jwt, Scheduller 
--- web/src/main/java/pl/com/app/WebApp.java
+++ web/src/main/java/pl/com/app/WebApp.java
@@ -3,12 +3,11 @@ package pl.com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.hateoas.HypermediaHttpMessageConverterConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -16,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableJpaRepositories("pl.com.app")
 @EntityScan("pl.com.app")
 @ComponentScan("pl.com.app")
+@EnableScheduling
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class,args);
